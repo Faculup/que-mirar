317c151a1cde89029e05a8fadaee410cc698e42a
Facundo <faculup90@gmail.com>
2025-01-05 18:15:47 +0100

    Dark mode set up

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,23 +1,59 @@
-import { Component, inject } from '@angular/core';
-import { ModalService } from '../../services/modal.service';
+import { Component, inject, OnInit, Renderer2 } from '@angular/core';
+
 import { AuthService } from '../../services/auth.service';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [AsyncPipe, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css',
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-  modal = inject(ModalService);
-  auth = inject(AuthService);
+export class NavComponent implements OnInit {
+  modal: ModalService = inject(ModalService);
+  auth: AuthService = inject(AuthService);
+  private renderer = inject(Renderer2);
+
+  isDarkMode = false;
+
+  private isLocalStorageAvailable(): boolean {
+    try {
+      return typeof localStorage !== 'undefined';
+    } catch {
+      return false;
+    }
+  }
+
+  ngOnInit() {
+    // Check if localStorage is available before using it
+    if (this.isLocalStorageAvailable()) {
+      this.isDarkMode = localStorage.getItem('theme') === 'dark';
+      if (this.isDarkMode) {
+        this.renderer.addClass(document.body, 'dark');
+      }
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark');
+      if (this.isLocalStorageAvailable()) {
+        localStorage.setItem('theme', 'dark');
+      }
+    } else {
+      this.renderer.removeClass(document.body, 'dark');
+      if (this.isLocalStorageAvailable()) {
+        localStorage.setItem('theme', 'light');
+      }
+    }
+  }
 
   openModal($event: Event) {
     $event.preventDefault();
-
     this.modal.toggle('auth');
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Black palette
+        'primary-dark': '#161d31',
+        'secondary-dark': '#283046',
+        'accent-dark': '#591bc5',
+
+        // White palette
+        'primary-light': '#ffffff',
+        'secondary-light': '#f8f9fa',
+        'accent-light': '#007bff',
+      },
+    },
   },
   plugins: [],
+  darkMode: 'class', // Enable dark mode via class
 }
